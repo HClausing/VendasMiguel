019a7f83f4d99b52575271a67c3008a7299d4456
Henrique Clausing <hclausing@hotmail.com>
2024-08-10 16:37:24 -0300

    fix

--- VendasMiguel/Models/OrdemItem.cs
+++ VendasMiguel/Models/OrdemItem.cs
@@ -4,5 +4,5 @@ public class OrdemItem
 {
     public Produto? Produto { get; set; }
     public decimal? Quantidade { get; set; }
-    public decimal? Total { get; set;
+    public decimal? Total { get; set; }
 }
